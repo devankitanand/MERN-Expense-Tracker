a848138af56269d385ccf1a8a182cd5a9a7bce94
Ankit Anand <devankitanand@gmail.com>
2025-01-30 00:44:36 +0530

    bug fix

--- server/server.js
+++ server/server.js
@@ -1,26 +1,31 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import connect from "./database/mongodb.js";
-import passport from "passport";
-import passportConfig from "./config/passport.js";
-import * as dotenv from "dotenv";
-import routes from "./routes/index.js";
+import express from 'express';
+import cors from 'cors';
+import connect from './database/mongodb.js';
+import passport from 'passport';
+import passportConfig from './config/passport.js';
+import * as dotenv from 'dotenv';
+import routes from './routes/index.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors({
+  origin: 'https://mern-expense-tracker-one.vercel.app',
+}));
+app.use(express.json());
 app.use(passport.initialize());
 passportConfig(passport);
 
-app.use("/", routes);
+app.use('/', routes);
 
-await connect();
-
-app.listen(PORT, () => {
-  console.log("Server is running at http://localhost:4000");
-});
+connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
